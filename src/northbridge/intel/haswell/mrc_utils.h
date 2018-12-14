056d71493fbd3ba4d7038783e0e4f3e153ef6d3c
Iru Cai <mytbk920423@gmail.com>
2018-12-14 18:39:24 +0800

    move util functions to mrc_utils.h

---
+++ src/northbridge/intel/haswell/mrc_utils.h
@@ -0,0 +1,19 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+void __attribute((regparm(3))) mrc_setmem(void *s, size_t n, int c);
+void __attribute((regparm(3))) mrc_memcpy(void *dst, const void *src, size_t n);
+void __attribute((regparm(3))) mrc_fillword(int *s, int c, size_t n);
+void __attribute((regparm(3))) mrc_zeromem(void *s, size_t n);
+char __attribute((regparm(3))) mrc_highest_bit(int a);
+void __attribute((regparm(1))) printGuid(const void *g);
