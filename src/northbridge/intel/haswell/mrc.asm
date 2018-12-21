ae6e179fa684dfec7dd03056d6de9096efdbc504
Iru Cai <mytbk920423@gmail.com>
2018-12-21 15:20:34 +0800

    mrc_end_of_pei

--- src/northbridge/intel/haswell/mrc.asm
+++ src/northbridge/intel/haswell/mrc.asm
@@ -127,7 +127,12 @@ global ref_fffcb998
 global ref_fffcb99c
 global ref_fffcc988
 
-;;
+;; mrc_end_of_pei
+
+extern ref_fffcd554
+extern ref_fffcd560
+global wstr_pchinitpei
+
 
 mrc_entry:
 mov ecx, esp
@@ -1481,23 +1486,6 @@ pop edi
 pop ebp
 ret
 
-fcn_fffa1227:  ; not directly referenced
-push ebp
-mov ebp, esp
-lea esp, [esp - 8]
-call haswell_stepping
-call haswell_family_model
-mov edx, 4
-mov eax, ref_fffcd4a4  ; mov eax, 0xfffcd4a4
-call fcn_fffa11b3  ; call 0xfffa11b3
-test eax, eax
-je short loc_fffa124f  ; je 0xfffa124f
-mov byte [eax + 0x49], 0
-
-loc_fffa124f:  ; not directly referenced
-xor eax, eax
-leave
-ret
 
 fcn_fffa1d20:
 push ebp
@@ -46760,49 +46748,6 @@ xor eax, eax
 pop ebp
 ret
 
-fcn_fffc7c34:  ; not directly referenced
-push ebp
-mov ebp, esp
-push ebx
-lea esp, [esp - 0x24]
-lea eax, [ebp - 0x1c]
-mov ebx, dword [ebp + 8]
-call PeiServiceGetBootMode  ; call 0xfffbf908
-cmp dword [ebp - 0x1c], 0x11
-jne short loc_fffc7c96  ; jne 0xfffc7c96
-sub esp, 0xc
-lea edx, [ebp - 0x14]
-mov eax, dword [ebx]
-push edx
-push 0
-push 0
-push gEfiPeiReadOnlyVariablePpiGuid
-push ebx
-call dword [eax + 0x20]  ; ucall
-add esp, 0x18
-lea eax, [ebp - 0xe]
-push eax
-lea eax, [ebp - 0x18]
-mov dword [ebp - 0x18], 6
-push eax
-mov eax, dword [ebp - 0x14]
-push 0
-push ref_fffcd56c  ; push 0xfffcd56c
-push wstr_pchinitpei
-push ebx
-call dword [eax]  ; ucall
-add esp, 0x20
-test eax, eax
-js short loc_fffc7c96  ; js 0xfffc7c96
-mov dx, word [ebp - 0xe]
-xor eax, eax
-out dx, ax
-
-loc_fffc7c96:  ; not directly referenced
-xor eax, eax
-mov ebx, dword [ebp - 4]
-leave
-ret
 
 fcn_fffc8290:
 push ebp
@@ -52722,25 +52667,3 @@ dd 0xed097352
 dd 0x445a9041
 dd 0x9db2b680
 dd 0x45889e50
-
-ref_fffcd554:
-dd 0x80000020
-dd gEfiEndOfPeiSignalPpiGuid
-dd fcn_fffa1227
-
-ref_fffcd560:
-dd 0x80000020
-dd gEfiEndOfPeiSignalPpiGuid
-dd fcn_fffc7c34
-
-ref_fffcd56c:
-dd 0xa31b27a4
-dd 0x48ffcae6
-dd 0x42295a8c
-dd 0x89f3e621
-
-gEfiEndOfPeiSignalPpiGuid:
-dd 0x605ea650
-dd 0x42e1c65c
-dd 0xa59180ba
-dd 0xc618b62a
