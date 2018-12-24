4cf6e1c154d8ede2107f759f4e579795d34c995b
Iru Cai <mytbk920423@gmail.com>
2018-12-24 20:12:09 +0800

    is_{desktop,mobile}_pch

--- src/northbridge/intel/haswell/mrc_sku.c
+++ src/northbridge/intel/haswell/mrc_sku.c
@@ -44,3 +44,25 @@ uint8_t mrc_pch_revision(void)
 		return 7;
 	}
 }
+
+int is_desktop_pch(uint16_t did)
+{
+	uint16_t t1 = did & 0xfffd;
+	if (t1 == 0x8c44 /* Z87, Z85 */ || t1 == 0x8c4c /* Q87, Q85 */)
+		return 1;
+
+	if (did == 0x8c5c /* H81 */ || did == 0x8c50 /* B85 */)
+		return 1;
+
+	t1 = did & 0xfff7;
+	if (t1 == 0x8c42 /* desktop ES or H87 */)
+		return 1;
+
+	return 0;
+}
+
+int is_mobile_pch(uint16_t did)
+{
+	return (did == 0x8c4f || did == 0x8c49 ||
+			did == 0x8c4b || did == 0x8c41);
+}
