2275cff4886350d377e07eb3fc00b138f3b71d6d
Iru Cai <mytbk920423@gmail.com>
2019-01-02 16:24:03 +0800

    frag_usb_fffaf555

--- src/northbridge/intel/haswell/mrc_frags.c
+++ src/northbridge/intel/haswell/mrc_frags.c
@@ -800,6 +800,18 @@ void set_usb_overcurrent(PEI_USB *upd)
 	}
 }
 
+void frag_usb_fffaf555(PEI_USB *upd, int count);
+void frag_usb_fffaf555(PEI_USB *upd, int count)
+{
+	u32 v = 0;
+	for (int i = 0; i < count; i++) {
+		if (upd->xhci_resume_info[0x11 + i] == 1) {
+			v |= (1 << i);
+		}
+	}
+	pci_update_config32(PCI_DEV(0, 0x14, 0), 0xd8, 0xffffffc0, v);
+}
+
 void frag_usb_fffaf75b(PEI_USB *upd);
 void frag_usb_fffaf75b(PEI_USB *upd)
 {
